8f0407b1daf2220e0a3a201dafa003f8cfb66885
max <sebastian.8295.8295@gmail.com>
2023-10-12 08:33:57 +0100

    Init commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [./nixos/configuration.nix];
+      };
+    };
+
+    homeConfigurations = {
+      "max@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [./home-manager/home.nix];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+  ];
+
+  nixpkgs = {
+    overlays = [
+    ];
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "max";
+    homeDirectory = "/home/max";
+  };
+
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "23.05";
+}
